ab4a1c416ae1893fb32d567ebe3c0a8f8ed8f194
ziyi-bear <m0724001@gm.nuu.edu.tw>
2021-02-17 14:32:35 +0800

    complete web page show db different type item count

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-
+# 如果檢測到URL模式，將調用一個視圖函數：views.index,  它是views.py文件中名為index() 的函數。 
 urlpatterns = [
-
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
 ]
